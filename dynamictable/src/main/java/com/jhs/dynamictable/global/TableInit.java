138a76e69ec367ee9baf428c5152318c1bb599a6
Juhongseok <wnghdtjr129@naver.com>
2024-09-23 21:39:32 +0900

    feat: bulk insert 적용

--- dynamictable/src/main/java/com/jhs/dynamictable/global/TableInit.java
+++ dynamictable/src/main/java/com/jhs/dynamictable/global/TableInit.java
@@ -1,26 +1,50 @@
 package com.jhs.dynamictable.global;
 
-import com.jhs.dynamictable.history.model.History;
-import jakarta.persistence.EntityManager;
+import com.jhs.dynamictable.global.support.dto.TableNameMetadata;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Transactional
 @RequiredArgsConstructor
 public class TableInit implements ApplicationRunner {
 
-    private final EntityManager em;
+    private final JdbcTemplate jdbcTemplate;
+    private final TableNameGenerator tableNameGenerator;
 
     @Override
     public void run(ApplicationArguments args) {
-        for (int i = 0; i < 10; i++) {
-            History history = new History("content" + i);
-            em.persist(history);
-        }
+        TableNameMetadata metadata = tableNameGenerator.generateTableName();
+        String tableName = metadata.generatedName();
+
+        List<HistoryDto> data = List.of(
+                new HistoryDto(1L, "content1"),
+                new HistoryDto(2L, "content2"),
+                new HistoryDto(3L, "content3"),
+                new HistoryDto(4L, "content4"),
+                new HistoryDto(5L, "content5"),
+                new HistoryDto(6L, "content6"),
+                new HistoryDto(7L, "content7")
+        );
+
+        jdbcTemplate.batchUpdate("""
+                insert into %s (id, content)
+                values (?, ?)
+                """.formatted(tableName), data, 10, (ps, argument) -> {
+            ps.setLong(1, argument.id);
+            ps.setString(2, argument.content);
+        });
+
+    }
+
+    public record HistoryDto(Long id, String content) {
+
     }
 
 }
