f997f93cc646ea0be4793e2f318b149d0bddbf02
JuHongseok <wnghdtjr129@naver.com>
2024-12-02 20:57:16 +0900

    feat: 절차적 설계

---
+++ inf-object/src/reservation/persistence/DiscountPolicyDAO.java
@@ -0,0 +1,9 @@
+package reservation.persistence;
+
+import reservation.domain.DiscountPolicy;
+
+public interface DiscountPolicyDAO {
+    DiscountPolicy selectDiscountPolicy(Long movieId);
+
+    void insert(DiscountPolicy discountPolicy);
+}
---
+++ inf-object/src/reservation/persistence/memory/DiscountConditionMemoryDAO.java
@@ -0,0 +1,13 @@
+package reservation.persistence.memory;
+
+import reservation.domain.DiscountCondition;
+import reservation.persistence.DiscountConditionDAO;
+
+import java.util.List;
+
+public class DiscountConditionMemoryDAO extends InMemoryDAO<DiscountCondition> implements DiscountConditionDAO {
+    @Override
+    public List<DiscountCondition> selectDiscountConditions(Long policyId) {
+        return findMany(condition -> condition.getPolicyId().equals(policyId));
+    }
+}
