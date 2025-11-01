9858d82ddd8335bfcffb9e5304111da76d070860
Juhongseok <wnghdtjr129@naver.com>
2025-11-01 12:28:58 +0900

    init toby spring

---
+++ inf-toby-spring/src/test/java/com/jhs/toby_spring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhs.toby_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
